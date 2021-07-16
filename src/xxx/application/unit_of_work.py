d4ce02c1ceffe44a7f324ab496e7360bedac9e83
warren-wupeng <warren.wu@visionwx.com>
2021-07-16 10:10:22 +0800

    Create unit_of_work.py

---
+++ src/xxx/application/unit_of_work.py
@@ -0,0 +1,28 @@
+# pylint: disable=attribute-defined-outside-init
+from __future__ import annotations
+import abc
+
+class AbstractUnitOfWork(abc.ABC):
+    products: repository.AbstractRepository
+
+    def __enter__(self) -> AbstractUnitOfWork:
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    def commit(self):
+        self._commit()
+
+    def collect_new_events(self):
+        for product in self.products.seen:
+            while product.events:
+                yield product.events.pop(0)
+
+    @abc.abstractmethod
+    def _commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
